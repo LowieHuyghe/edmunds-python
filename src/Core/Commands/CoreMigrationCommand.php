19dda4fd9d96f02dbe900fa342226810f5ab5eb9
Lowie Huyghe <LowieHuyghe@users.noreply.github.com>
2015-06-12 23:12:35 +0200

    Custom Migration: Migration command adaption

--- src/Core/Commands/CoreMigrationCommand.php
+++ src/Core/Commands/CoreMigrationCommand.php
@@ -12,6 +12,7 @@
  */
 
 namespace LH\Core\Commands;
+use LH\Core\Database\Migrations\BaseMigration;
 
 /**
  * CoreMigration class responsible for updating the database
@@ -24,54 +25,241 @@ namespace LH\Core\Commands;
 class CoreMigrationCommand extends BaseCommand
 {
 	/**
-	 * The console command name.
+	 * The command name and signature.
 	 * @var string
 	 */
-	protected $name = 'coremigrate';
+	protected $signature = 'coremigrate:{option}';
 
 	/**
 	 * The console command description.
 	 * @var string
 	 */
-	protected $description = 'The class responsable to run all migrations.';
+	protected $description = 'The class responsible to run all migrations.';
 
 	/**
 	 * All the tables that can be migrated
-	 * @var string[]
+	 * @var BaseMigration[]
 	 */
-	private $classes;
+	private static $classes = array();
 
+	/**
+	 * Add a class for the migrations
+	 * @param BaseMigration $instance
+	 */
+	public static function addClass($instance)
+	{
+		self::$classes[] = $instance;
+	}
 
 	/**
 	 * Execute the console command.
 	 * @return mixed
 	 */
-	public function fire()
+	public function handle()
 	{
-		$allversions = $this->option('allversions');
-		$currentversions = $this->option('currentversions');
-		$goto = $this->option('goto');
-		$update = $this->option('update');
-		$rollback = $this->option('rollback');
+		$allVersions = $this->option('allversions');
+		$currentVersions = $this->option('currentversions');
+
+		$upgrade = $this->option('upgrade');
+		$downgrade = $this->option('downgrade');
 		$refresh = $this->option('refresh');
+
 		$seed = $this->option('seed');
 
-		if ($allversions) {
+		if ($allVersions)
+		{
 			$this->printAvailableVersions();
-		} else if ($currentversions) {
+		}
+		else if ($currentVersions)
+		{
 			$this->printCurrentVersion();
-		} else if ($goto) {
-			$this->update($goto);
-		} else if ($update) {
-			$this->update();
-		} else if ($rollback) {
-			$this->rollback();
-		} else if ($refresh) {
+		}
+		else if ($upgrade)
+		{
+			$this->upgrade();
+		}
+		else if ($downgrade)
+		{
+			$this->downgrade();
+		}
+		else if ($refresh)
+		{
 			$this->refresh();
-		} else if ($seed) {
+		}
+		else if ($seed)
+		{
 			$this->seed();
-		} else {
+		}
+		else
+		{
 			$this->info('No input detected. Try --help (-h) for more info.');
 		}
+	}
+
+	/**
+	 * Upgrade the database
+	 */
+	private function upgrade()
+	{
+		$this->info('Initiating upgrading database from ' . implode(', ', array_flatten($this->getCurrentVersions())));
+		$newVersion = $this->ask('To what version may I ask? (' . implode(', ', $this->getAvailableVersions()) . ')');
+		if (!in_array($newVersion, $this->getAvailableVersions()))
+		{
+			$this->info('No valid version specified.');
+			return;
+		}
+
+		$updatedSomething = false;
+		foreach ($this->classes as $schema)
+		{
+			if ($schema->up($newVersion))
+			{
+				$this->info("Upgraded " . get_class($schema) . " to version $newVersion");
+				$updatedSomething = true;
+			}
+		}
+
+		//Print outcome
+		if ($updatedSomething) {
+			$this->info("Successfully upgraded database to $newVersion");
+		} else {
+			$this->info('Nothing to upgrade.');
+		}
+	}
+
+	/**
+	 * Downgrade the database
+	 */
+	private function downgrade()
+	{
+		$this->info('Initiating downgrading database from ' . implode(', ', array_flatten($this->getCurrentVersions())));
+		$newVersion = $this->ask('To what version may I ask? (' . implode(', ', $this->getAvailableVersions()) . ')');
+		if (!in_array($newVersion, $this->getAvailableVersions()))
+		{
+			$this->info('No valid version specified.');
+			return;
+		}
+
+		$updatedSomething = false;
+		foreach ($this->classes as $schema)
+		{
+			if ($schema->down($newVersion))
+			{
+				$this->info("Upgraded " . get_class($schema) . " to version $newVersion");
+				$updatedSomething = true;
+			}
+		}
+
+		//Print outcome
+		if ($updatedSomething) {
+			$this->info("Successfully downgraded database to $newVersion");
+		} else {
+			$this->info('Nothing to downgrade.');
+		}
+	}
+
+	/**
+	 * Delete the whole database and update it again.
+	 */
+	private function refresh()
+	{
+		$versions = $this->getAvailableVersions();
+
+		if (!empty($versions))
+		{
+			$this->downgrade('0');
+			$this->upgrade($versions[count($versions)-1]);
+		}
+	}
+
+	/**
+	 * Seed the database with init-data.
+	 */
+	private function seed()
+	{
+		if (!$this->confirm('Are you sure you want to seed the database (and thus delete the records in those tables)? [yes|no]', false)) {
+			return false;
+		}
+
+		$this->info('Started seeding the database');
+
+		$init = new InitSeeder();
+		$init->run();
+
+		$this->info('Successfully seeded the database');
+	}
+
+	/**
+	 * Print the current version
+	 */
+	private function printCurrentVersion()
+	{
+		$versions = $this->getCurrentVersions();
+
+		if ($versions) {
+			$this->info('The database is currently at version' . (count($versions) ? 's' : '') . ': ' . implode(', ', array_flatten($versions)));
+		} else {
+			$this->info('The database has not yet been initiated.');
+		}
+	}
+
+	/**
+	 * Return the current versions in the db
+	 * @return string[]
+	 */
+	private function getCurrentVersions()
+	{
+		$versions = DB::select('SELECT version FROM core_migrations GROUP BY version');
+		return BaseMigration::sortVersions($versions);
+	}
+
+	/**
+	 * Print the available versions of the database
+	 */
+	private function printAvailableVersions()
+	{
+		$this->info('Available versions: ');
+
+		$versions = $this->getAvailableVersions();
+		$this->info(implode(', ', $versions));
+	}
+
+	/**
+	 * Return the available versions
+	 * @return string[]
+	 */
+	private function getAvailableVersions()
+	{
+		//Fetch
+		$versions = array();
+		foreach ($this->classes as $schema) {
+			$versions = array_merge($versions, $schema->getAllVersions());
+		}
+
+		//Sort
+		if (!empty($versions))
+		{
+			BaseMigration::sortVersions($versions);
+		}
+
+		return $versions;
+	}
+
+	/**
+	 * Get the console command options.
+	 * @return array
+	 */
+	protected function getOptions()
+	{
+		return array(
+			array('allversions', 'av', InputOption::VALUE_NONE, 'Print the available database versions.', null),
+			array('currentversions', 'cv', InputOption::VALUE_NONE, 'Print the current database version.', null),
+
+			array('upgrade', 'ug', InputOption::VALUE_NONE, 'Upgrade the database to the specified version (v).', null),
+			array('downgrade', 'dg', InputOption::VALUE_NONE, 'Downgrade the database to the specified version (v).', null),
+			array('refresh', 're', InputOption::VALUE_NONE, 'Delete the whole database and create it again.', null),
+
+			array('seed', 'se', InputOption::VALUE_NONE, 'Seed the database with init-data.', null),
+		);
 	}
 }
