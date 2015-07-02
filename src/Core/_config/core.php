da2d0212a638d0ec034abb472f1f923df177b3b9
Lowie Huyghe <iam@lowiehuyghe.com>
2015-07-02 19:19:36 +0200

    CORE-34: Finished generalized config

---
+++ src/Core/_config/core.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * LH Core
+ *
+ * The core of any web-project by Lowie Huyghe
+ *
+ * @author		Lowie Huyghe <iam@lowiehuyghe.com>
+ * @copyright	Copyright (C) 2015, Lowie Huyghe. All rights reserved. Unauthorized copying of this file, via any medium is strictly prohibited. Proprietary and confidential.
+ * @license		http://LicenseUrl
+ * @since		Version 0.1
+ */
+
+/**
+ * A configuration file
+ *
+ * @author		Lowie Huyghe <iam@lowiehuyghe.com>
+ * @copyright	Copyright (C) 2015, Lowie Huyghe. All rights reserved. Unauthorized copying of this file, via any medium is strictly prohibited. Proprietary and confidential.
+ * @license		http://LicenseUrl
+ * @since		Version 0.1
+ */
+
+return array(
+
+	'config' => [
+		'required' => [
+			'app.routing.namespace',
+			'app.routing.default',
+			'app.routing.home',
+			'app.routing.loginroute',
+			'app.routing.redirecthalt',
+			'app.routing.redirecthalt',
+		],
+	],
+
+	'pm' => [
+		'pushbullet' => [
+			'account' => 'huyghe.lowie@gmail.com',
+			'token' => 'jue2B1zhgiU6cIwxAc55vY0rKfjL5iC5',
+		],
+	],
+
+);
