0285caa84bddd0fa04dfb51f4102b10d0ee0cf21
Lowie Huyghe <LowieHuyghe@users.noreply.github.com>
2015-07-22 03:48:17 +0200

    Additional comments to updategeoip-command.

--- src/Core/Commands/UpdateGeoIP.php
+++ src/Core/Commands/UpdateGeoIP.php
@@ -1,10 +1,29 @@
 <?php
 
+/**
+ * LH Core
+ *
+ * The core of any web-project by Lowie Huyghe
+ *
+ * @author		Lowie Huyghe <iam@lowiehuyghe.com>
+ * @copyright	Copyright (C) 2015, Lowie Huyghe. All rights reserved. Unauthorized copying of this file, via any medium is strictly prohibited. Proprietary and confidential.
+ * @license		http://LicenseUrl
+ * @since		Version 0.1
+ */
+
 namespace LH\Core\Commands;
 
 use LH\Core\Helpers\LocationHelper;
 use LH\Core\Helpers\PmHelper;
 
+/**
+ * The command for updating the geoip-databases. Please shedule for every week.
+ *
+ * @author		Lowie Huyghe <iam@lowiehuyghe.com>
+ * @copyright	Copyright (C) 2015, Lowie Huyghe. All rights reserved. Unauthorized copying of this file, via any medium is strictly prohibited. Proprietary and confidential.
+ * @license		http://LicenseUrl
+ * @since		Version 0.1
+ */
 class UpdateGeoIP extends BaseCommand
 {
 	/**
