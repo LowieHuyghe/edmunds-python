b3c4974e6433414cadaffbd7497c9e795b4d5291
Lowie Huyghe <iam@lowiehuyghe.com>
2015-10-09 21:58:52 +0200

    #56: Added basic authentication. #55: Added AbortWithResponseException. Added login and logout functions to visitor.

---
+++ src/Core/Exceptions/AbortWithResponseException.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Core
+ *
+ * The core of any web-project by Lowie Huyghe
+ *
+ * @author		Lowie Huyghe <iam@lowiehuyghe.com>
+ * @copyright	Copyright (C) 2015, Lowie Huyghe. All rights reserved. Unauthorized copying of this file, via any medium is strictly prohibited. Proprietary and confidential.
+ * @license		http://LicenseUrl
+ * @since		Version 0.1
+ */
+
+namespace Core\Exceptions;
+
+/**
+ * Exception to stop the flow of the app and finish but do send response (instead of die and exit so logging can be done)
+ *
+ * @author		Lowie Huyghe <iam@lowiehuyghe.com>
+ * @copyright	Copyright (C) 2015, Lowie Huyghe. All rights reserved. Unauthorized copying of this file, via any medium is strictly prohibited. Proprietary and confidential.
+ * @license		http://LicenseUrl
+ * @since		Version 0.1
+ */
+class AbortWithResponseException extends BaseException
+{
+	//
+}
