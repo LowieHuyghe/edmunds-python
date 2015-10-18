c613204ec3033aba8f9089938f7938e5531ccf48
Lowie Huyghe <iam@lowiehuyghe.com>
2015-10-19 01:43:30 +0200

    #55: Moved auth stuff to dedicated structure.

---
+++ src/Core/Structures/Auth.php
@@ -0,0 +1,166 @@
+<?php
+
+/**
+ * Core
+ *
+ * The core of any web-project by Lowie Huyghe
+ *
+ * @author		Lowie Huyghe <iam@lowiehuyghe.com>
+ * @copyright	Copyright (C) 2015, Lowie Huyghe. All rights reserved. Unauthorized copying of this file, via any medium is strictly prohibited. Proprietary and confidential.
+ * @license		http://LicenseUrl
+ * @since		Version 0.1
+ */
+
+namespace Core\Structures;
+use Core\Models\User;
+use Core\Structures\Client\Visitor;
+
+/**
+ * The structure for authentication
+ *
+ * @author		Lowie Huyghe <iam@lowiehuyghe.com>
+ * @copyright	Copyright (C) 2015, Lowie Huyghe. All rights reserved. Unauthorized copying of this file, via any medium is strictly prohibited. Proprietary and confidential.
+ * @license		http://LicenseUrl
+ * @since		Version 0.
+ *
+ * @property bool $loggedIn
+ * @property User $user
+ */
+class Auth extends BaseStructure
+{
+	/**
+	 * Instance of the auth-structure
+	 * @var Auth
+	 */
+	private static $instance;
+
+	/**
+	 * Fetch instance of the auth-structure
+	 * @return Auth
+	 */
+	public static function current()
+	{
+		if (!isset(self::$instance))
+		{
+			self::$instance = new Auth();
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * @var User
+	 */
+	private $loggedInUser = null;
+
+	/**
+	 * Check if visitor is logged in
+	 * @return bool
+	 */
+	protected function getLoggedInAttribute()
+	{
+		return app('auth')->check();
+	}
+
+	/**
+	 * Get the current logged in user
+	 * @return User
+	 */
+	protected function getUserAttribute()
+	{
+		if ($this->loggedIn) //Logged in
+		{
+			$authUser = app('auth')->user(); //Get user
+
+			if (!$this->loggedInUser //There was no logged in user
+				|| $this->loggedInUser->id != $authUser->id) //Or userId does not match
+			{
+				if ($user = User::find($authUser->id)) //Find user
+				{
+					$this->loggedInUser = $user;
+				}
+				else
+				{
+					$this->loggedInUser = null;
+					$this->logout();
+				}
+			}
+		}
+		elseif ($this->loggedInUser) //Not logged in, but loggedInUser is set
+		{
+			$this->loggedInUser = null;
+		}
+
+		return $this->loggedInUser;
+	}
+
+	/**
+	 * Log a user in
+	 * @param string $email
+	 * @param string $password
+	 * @param bool $once
+	 * @return bool
+	 */
+	public function loginWithCredentials($email, $password = null, $once = false)
+	{
+		$credentials = array('email' => $email, 'password' => $password);
+
+		if ($once)
+		{
+			$loggedIn = app('auth')->once($credentials);
+		}
+		else
+		{
+			$loggedIn = app('auth')->attempt($credentials);
+		}
+
+		if ($loggedIn)
+		{
+			$user = User::where('email', '=', $email);
+
+			$this->loggedInUser = $user;
+			Visitor::current()->user = $user;
+
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Log a user in
+	 * @param User $user
+	 * @param bool $once
+	 * @return bool
+	 */
+	public function loginWithUser($user, $once = false)
+	{
+		if ($once || app('auth')->login($user))
+		{
+			$this->loggedInUser = $user;
+			Visitor::current()->user = $user;
+
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Log the current user out
+	 * @return bool
+	 */
+	public function logout()
+	{
+		if (app('auth')->logout())
+		{
+			$this->loggedInUser = null;
+			Visitor::current()->user = null;
+
+			return true;
+		}
+
+		return false;
+	}
+
+}
