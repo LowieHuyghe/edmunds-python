3b82adfdd6b2966f43bc0ec39d59c977677137c7
Lowie Huyghe <iam@lowiehuyghe.com>
2015-11-12 20:31:26 +0100

    #55: Last bugfixes fof moving the stuff to lower levels.

--- src/Core/Exceptions/Handler.php
+++ src/Core/Exceptions/Handler.php
@@ -13,86 +13,61 @@ use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * A list of the exception types that should not be reported.
-     *
-     * @var array
-     */
-    protected $dontReport = [
-        HttpException::class,
-    ];
+	/**
+	 * A list of the exception types that should not be reported.
+	 *
+	 * @var array
+	 */
+	protected $dontReport = [
+		HttpException::class,
+	];
 
-    /**
-     * Report or log an exception.
-     *
-     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
-     *
-     * @param  \Exception  $e
-     * @return void
-     */
-    public function report(Exception $e)
-    {
-        return parent::report($e);
-    }
+	/**
+	 * Report or log an exception.
+	 *
+	 * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+	 *
+	 * @param  \Exception  $e
+	 * @return void
+	 */
+	public function report(Exception $e)
+	{
+		return parent::report($e);
+	}
 
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $e
-     * @return \Illuminate\Http\Response
-     */
-    public function render($request, Exception $e)
-    {
-        if (!env('APP_DEBUG'))
-        {
-            if ($e instanceof UnauthorizedHttpException)
-            {
-                $response = Response::current();
+	/**
+	 * Render an exception into an HTTP response.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Exception  $e
+	 * @return \Illuminate\Http\Response
+	 */
+	public function render($request, Exception $e)
+	{
+		if (!env('APP_DEBUG'))
+		{
+			if ($e instanceof UnauthorizedHttpException
+				|| $e instanceof AccessDeniedHttpException
+				|| $e instanceof NotFoundHttpException
+				|| $e instanceof ServiceUnavailableHttpException)
+			{
+				$response = Response::current();
 
-                $response->assignHeader($e->getHeaders());
-                $response->assignView(null, 'errors.401');
-                $response->assign('message', $e->getMessage());
-                $response->setType(Response::TYPE_401);
+				$response->assignHeader($e->getHeaders());
+				$response->assignView(null, 'errors.' . $e->getStatusCode());
+				$response->assign('message', $e->getMessage());
+				$response->setStatusCode($e->getStatusCode());
+				$response->setType(Response::TYPE_VIEW);
 
-                return $response->getResponse();
-            }
-            elseif ($e instanceof AccessDeniedHttpException)
-            {
-                $response = Response::current();
+				if ($e instanceof ServiceUnavailableHttpException)
+				{
+					$response->assign('maintenance', app()->isDownForMaintenance());
+				}
 
-                $response->assignHeader($e->getHeaders());
-                $response->assignView(null, 'errors.403');
-                $response->assign('message', $e->getMessage());
-                $response->setType(Response::TYPE_403);
+				return $response->getResponse();
+			}
+		}
 
-                return $response->getResponse();
-            }
-            elseif ($e instanceof NotFoundHttpException)
-            {
-                $response = Response::current();
-
-                $response->assignHeader($e->getHeaders());
-                $response->assignView(null, 'errors.404');
-                $response->assign('message', $e->getMessage());
-                $response->setType(Response::TYPE_404);
-
-                return $response->getResponse();
-            }
-            elseif ($e instanceof ServiceUnavailableHttpException)
-            {
-                $response = Response::current();
-
-                $response->assignHeader($e->getHeaders());
-                $response->assignView(null, 'errors.503');
-                $response->assign('message', $e->getMessage());
-                $response->assign('maintenance', app()->isDownForMaintenance());
-                $response->setType(Response::TYPE_503);
-
-                return $response->getResponse();
-            }
-        }
-
-        return parent::render($request, $e);
-    }
+		return parent::render($request, $e);
+	}
 }
