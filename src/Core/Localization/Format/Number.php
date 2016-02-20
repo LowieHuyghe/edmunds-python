c69603fac31628232888af2dd674193f8d87dae6
Lowie Huyghe <iam@lowiehuyghe.com>
2016-02-20 22:42:42 +0100

    #84: Added functions to format length and metric. Still needs to be implemented.

--- src/Core/Localization/Format/Number.php
+++ src/Core/Localization/Format/Number.php
@@ -28,68 +28,82 @@ use NumberFormatter;
 class Number extends BaseStructure
 {
 
-    /**
-     * Constructor
-     * @param mixed $number
-     * @param string|null $locale
-     * @param string|null $currency
-     */
-    public function __construct($number, $locale = null, $currency = null)
-    {
-        $this->number = $number;
+	/**
+	 * Constructor
+	 * @param mixed $number
+	 * @param string|null $locale
+	 * @param string|null $currency
+	 */
+	public function __construct($number, $locale = null, $currency = null)
+	{
+		$this->number = $number;
 
-        $localization = Visitor::getInstance()->localization;
-        $this->locale = $locale ?: $localization->locale;
-        $this->currency = $currency ?: $localization->currency;
-    }
+		$localization = Visitor::getInstance()->localization;
+		$this->locale = $locale ?: $localization->locale;
+		$this->currency = $currency ?: $localization->currency;
+	}
 
-    /**
-     * Format a number
-     * @param  mixed $number
-     * @return string
-     */
-    public function format()
-    {
-        $formatter = new NumberFormatter($this->locale, NumberFormatter::DECIMAL);
-        $formatter->setAttribute(NumberFormatter::MIN_FRACTION_DIGITS, 0);
-        $formatter->setAttribute(NumberFormatter::MAX_FRACTION_DIGITS, 100);
+	/**
+	 * Format a number
+	 * @return string
+	 */
+	public function format()
+	{
+		$formatter = new NumberFormatter($this->locale, NumberFormatter::DECIMAL);
+		$formatter->setAttribute(NumberFormatter::MIN_FRACTION_DIGITS, 0);
+		$formatter->setAttribute(NumberFormatter::MAX_FRACTION_DIGITS, 100);
 
-        return $formatter->format($this->number);
-    }
+		return $formatter->format($this->number);
+	}
 
-    /**
-     * Format a currency
-     * @param  mixed $number
-     * @return string
-     */
-    public function formatCurrency()
-    {
-        $formatter = new NumberFormatter($this->locale, NumberFormatter::CURRENCY);
+	/**
+	 * Format a currency
+	 * @return string
+	 */
+	public function formatCurrency()
+	{
+		$formatter = new NumberFormatter($this->locale, NumberFormatter::CURRENCY);
 
-        return $formatter->formatCurrency($this->number, $this->currency);
-    }
+		return $formatter->formatCurrency($this->number, $this->currency);
+	}
 
-    /**
-     * Format percentage
-     * @param  mixed $number
-     * @return string
-     */
-    public function formatPercent()
-    {
-        $formatter = new NumberFormatter($this->locale, NumberFormatter::PERCENT);
+	/**
+	 * Format percentage
+	 * @return string
+	 */
+	public function formatPercent()
+	{
+		$formatter = new NumberFormatter($this->locale, NumberFormatter::PERCENT);
 
-        return $formatter->format($this->number);
-    }
+		return $formatter->format($this->number);
+	}
 
-    /**
-     * Format scientific
-     * @param  mixed $number
-     * @return string
-     */
-    public function formatScientific()
-    {
-        $formatter = new NumberFormatter($this->locale, NumberFormatter::SCIENTIFIC);
+	/**
+	 * Format scientific
+	 * @return string
+	 */
+	public function formatScientific()
+	{
+		$formatter = new NumberFormatter($this->locale, NumberFormatter::SCIENTIFIC);
 
-        return $formatter->format($this->number);
-    }
+		return $formatter->format($this->number);
+	}
+
+	/**
+	 * Format length
+	 * @return string
+	 */
+	public function formatLength()
+	{
+		//
+	}
+
+	/**
+	 * Format as liquid
+	 * @return string
+	 */
+	public function formatLiquid()
+	{
+		//
+	}
 }
