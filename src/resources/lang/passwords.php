f49dfd92d7d57a2d9c4c7010af50aa902cf23c0c
Lowie Huyghe <LowieHuyghe@users.noreply.github.com>
2016-02-24 21:06:02 +0100

    #85: Implemented PasswordController. Modifications to AuthController. Still testing to do.

---
+++ src/resources/lang/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return array
+(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Passwords Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines contain the messages used by the
+	| password reset system.
+	|
+	*/
+
+	\Illuminate\Support\Facades\Password::RESET_LINK_SENT	=> 'passwords.sent',
+	\Illuminate\Support\Facades\Password::PASSWORD_RESET	=> 'passwords.reset',
+	\Illuminate\Support\Facades\Password::INVALID_USER		=> 'passwords.user',
+	\Illuminate\Support\Facades\Password::INVALID_PASSWORD	=> 'passwords.password',
+	\Illuminate\Support\Facades\Password::INVALID_TOKEN		=> 'passwords.token',
+	'passwords.email.subject'								=> 'Your Password Reset Link',
+);
