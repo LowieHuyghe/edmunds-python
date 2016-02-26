ba744b3330576fc8518249f9f93f1addb14bfbf3
Lowie Huyghe <LowieHuyghe@users.noreply.github.com>
2016-02-27 00:10:40 +0100

    #85: Also made model fillable

--- src/Core/Bases/Models/BaseModel.php
+++ src/Core/Bases/Models/BaseModel.php
@@ -56,6 +56,12 @@ class BaseModel extends Model
 	 */
 	protected $required = [];
 
+    /**
+     * The attributes that aren't mass assignable.
+     * @var array
+     */
+    protected $guarded = [];
+
 	/**
 	 * The attributes that should be mutated to dates.
 	 * @var array
