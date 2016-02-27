4f34a4d841cc4b687ca49db4a55cbedf0d45d3fc
Lowie Huyghe <iam@lowiehuyghe.com>
2016-02-27 17:15:00 +0100

    #85: Tested and fixed authcontroller

--- src/Core/Auth/Middleware/RedirectIfAuthenticated.php
+++ src/Core/Auth/Middleware/RedirectIfAuthenticated.php
@@ -13,56 +13,39 @@
 
 namespace Core\Auth\Middleware;
 
+use Closure;
 use Core\Auth\Auth;
-use Core\Auth\Guards\BasicStatefulGuard;
-use Core\Auth\Guards\BasicStatelessGuard;
 use Core\Bases\Http\Middleware\BaseMiddleware;
-use Core\Http\Request;
-use Core\Http\Response;
 
 /**
- * Middleware for authentication
+ * Redirect if authenticated middleware
  *
  * @author      Lowie Huyghe <iam@lowiehuyghe.com>
  * @copyright   Copyright (C) 2015, Lowie Huyghe. All rights reserved. Unauthorized copying of this file, via any medium is strictly prohibited. Proprietary and confidential.
  * @license     http://LicenseUrl
  * @since       Version 0.1
  */
-class AuthMiddleware extends BaseMiddleware
+class RedirectIfAuthenticated extends BaseMiddleware
 {
 	/**
 	 * Handle an incoming request.
-	 * @param  \Illuminate\Http\Request $r
+	 *
+	 * @param  \Illuminate\Http\Request  $request
 	 * @param  \Closure  $next
 	 * @return mixed
 	 */
-	public function handle($r, \Closure $next)
+	public function handle($request, Closure $next)
 	{
 		$auth = Auth::getInstance();
-		$request = Request::getInstance();
-		$response = Response::getInstance();
 
-		// check if guest
-		if (!$auth->loggedIn)
+		if ($auth->loggedIn)
 		{
-			$guard = $auth->getGuard();
-			if ($guard instanceof BasicStatefulGuard
-				|| $guard instanceof BasicStatelessGuard)
-			{
-				abort(401);
-			}
-			elseif ($request->ajax
-				|| $request->json
-				|| $request->xml)
-			{
-				abort(403);
-			}
-			else
-			{
-				$response->redirect(config('app.routing.loginroute'), true);
-			}
+			$response = Response::getInstance();
+
+			$response->redirect(config('app.auth.redirects.login', '/'));
+			return;
 		}
 
-		return $next($r);
+		return $next($request);
 	}
 }
