00e747db2ef09574f58168342b574728700f551e
Lowie Huyghe <iam@lowiehuyghe.com>
2016-03-02 22:13:13 +0100

    #76: Added redis warehouse in case this should be implemented.

---
+++ src/Core/Events/BroadcastEvent.php
@@ -0,0 +1,108 @@
+<?php
+
+/**
+ * Core
+ *
+ * The core of any web-project by Lowie Huyghe
+ *
+ * @author		Lowie Huyghe <iam@lowiehuyghe.com>
+ * @copyright	Copyright (C) 2015, Lowie Huyghe. All rights reserved. Unauthorized copying of this file, via any medium is strictly prohibited. Proprietary and confidential.
+ * @license		http://LicenseUrl
+ * @since		Version 0.1
+ */
+
+namespace Core\Events;
+
+use Core\Bases\Events\BaseBroadcastEvent;
+
+/**
+ * Broadcast to use
+ *
+ * @author		Lowie Huyghe <iam@lowiehuyghe.com>
+ * @copyright	Copyright (C) 2015, Lowie Huyghe. All rights reserved. Unauthorized copying of this file, via any medium is strictly prohibited. Proprietary and confidential.
+ * @license		http://LicenseUrl
+ * @since		Version 0.1
+ */
+class BroadcastEvent extends BaseBroadcastEvent
+{
+	/**
+	 * @var array
+	 */
+	protected $channels;
+
+	/**
+	 * @var array
+	 */
+	protected $payload;
+
+	/**
+	 * @var string
+	 */
+	protected $queue;
+
+	/**
+	 * @var string
+	 */
+	protected $eventName;
+
+	/**
+	 * Constructor
+	 * @param array $channels
+	 * @param array $payload
+	 */
+	public function __construct($channels = [], $payload = [], $queue = Queue::QUEUE_DEFAULT, $eventName = self::class)
+	{
+		$this->channels = $channels;
+		$this->payload = $payload;
+		$this->queue = $queue;
+		$this->eventName = $eventName;
+	}
+
+	/**
+	 * Broadcast the event
+	 */
+	public function broadcast()
+	{
+		event($this);
+	}
+
+	/**
+     * Get the channels the event should be broadcast on.
+     *
+     * @return array
+     */
+    public function broadcastOn()
+    {
+        return $this->channels;
+    }
+
+	/**
+	 * Get the data to broadcast.
+	 *
+	 * @return array
+	 */
+	public function broadcastWith()
+	{
+	    return $this->payload;
+	}
+
+	/**
+	 * Set the name of the queue the event should be placed on.
+	 *
+	 * @return string
+	 */
+	public function onQueue()
+	{
+	    return $this->queue;
+	}
+
+	/**
+	 * Get the broadcast event name.
+	 *
+	 * @return string
+	 */
+	public function broadcastAs()
+	{
+	    return $this->eventName;
+	}
+}
