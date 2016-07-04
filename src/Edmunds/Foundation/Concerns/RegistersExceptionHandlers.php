db9ab94707c004d4b796e74bfd6f4debff36e586
Lowie Huyghe <LowieHuyghe@users.noreply.github.com>
2016-07-04 20:40:06 +0200

    #109: Added custom loghandlers

--- src/Edmunds/Foundation/Concerns/RegistersExceptionHandlers.php
+++ src/Edmunds/Foundation/Concerns/RegistersExceptionHandlers.php
@@ -13,6 +13,10 @@ namespace Edmunds\Foundation\Concerns;
 use Edmunds\Http\Exceptions\AbortHttpException;
 use Edmunds\Http\Response;
 use Edmunds\Registry;
+use Monolog\Formatter\LineFormatter;
+use Monolog\Handler\StreamHandler;
+use Monolog\Handler\SyslogHandler;
+use Monolog\Logger;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -50,6 +54,41 @@ trait RegistersExceptionHandlers
 				throw new ServiceUnavailableHttpException(null, $message);
 			default:
 				throw new HttpException($code, $message, null, $headers);
+		}
+	}
+
+	/**
+	 * Get the Monolog handler for the application.
+	 *
+	 * @return \Monolog\Handler\AbstractHandler
+	 */
+	protected function getMonologHandler()
+	{
+		$monologHandler = config('app.exceptions.monologhandler', null);
+
+		// fetch class used for logging
+		if (is_array($monologHandler))
+		{
+			$monologHandlerClass = $monologHandler['class'];
+		}
+		else
+		{
+			$monologHandlerClass = $monologHandler;
+		}
+
+		switch($monologHandlerClass)
+		{
+			case SyslogHandler::class:
+				return (new SyslogHandler(
+					isset($monologHandler['ident']) ? $monologHandler['ident'] : null,
+					isset($monologHandler['facility']) ? $monologHandler['facility'] : LOG_USER,
+					Logger::DEBUG))
+						->setFormatter(new LineFormatter(null, null, true, true));
+
+			case StreamHandler::class:
+			default:
+				return parent::getMonologHandler();
+
 		}
 	}
 }
