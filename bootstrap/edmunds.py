0d938552a044e8af21a23e0ded177f0040892d65
Lowie Huyghe <LowieHuyghe@users.noreply.github.com>
2016-10-22 17:44:31 +0200

    #134: Initialised the Application to handle a request with Flask.

---
+++ bootstrap/edmunds.py
@@ -0,0 +1,14 @@
+
+from src.Edmunds.Application import Application
+
+
+def bootstrap():
+	"""
+	Bootstrap the Application
+	:return: 	The bootstrapped application
+	:rtype:		Application
+	"""
+
+	app = Application()
+
+	return app
---
+++ main.py
@@ -0,0 +1,8 @@
+
+from bootstrap import edmunds
+
+app = edmunds.bootstrap()
+
+
+if __name__ == '__main__':
+	app.run()
