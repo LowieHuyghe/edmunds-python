9709b90e22b5d23b2c3661941ad8295875744c56
Lowie Huyghe <LowieHuyghe@users.noreply.github.com>
2016-10-24 02:35:55 +0200

    #145: Example service provider for app.

---
+++ app/Providers/MyServiceProvider.py
@@ -0,0 +1,14 @@
+
+from Edmunds.Support.ServiceProvider import ServiceProvider
+
+
+class MyServiceProvider(ServiceProvider):
+	"""
+	My Service Provider
+	"""
+
+	def register(self):
+		"""
+		Register the service provider
+		"""
+		pass
