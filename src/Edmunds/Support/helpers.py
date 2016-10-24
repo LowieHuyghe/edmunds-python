0cb9d9d8dd781d372b6daf767ceb265019316689
Lowie Huyghe <LowieHuyghe@users.noreply.github.com>
2016-10-24 02:30:48 +0200

    #145: A service provider should only be registered once.

--- src/Edmunds/Support/helpers.py
+++ src/Edmunds/Support/helpers.py
@@ -30,3 +30,15 @@ def getModuleAndClass(className):
 	className = parts[-1]
 
 	return (module, className)
+
+
+def getFullClassName(class_):
+	"""
+	Get full class name of class
+	:param class_: 	The class
+	:type  class_: 	class
+	:return: 		The full class name
+	:rtype: 		string
+	"""
+
+	return class_.__module__ + '.' + class_.__name__
