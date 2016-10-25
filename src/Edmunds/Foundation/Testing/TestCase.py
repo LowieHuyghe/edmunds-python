03f70cdf874725de83958b12a27362d78988ca16
Lowie Huyghe <LowieHuyghe@users.noreply.github.com>
2016-10-26 01:05:16 +0200

    #145: Added testCase class for tests to extend from

---
+++ src/Edmunds/Foundation/Testing/TestCase.py
@@ -0,0 +1,20 @@
+
+import unittest
+
+
+class TestCase(unittest.TestCase):
+	"""
+	A UnitTest Test Case
+	"""
+
+	def setUp(self):
+		"""
+		Set up the test case
+		"""
+		pass
+
+	def tearDown(self):
+		"""
+		Tear down the test case
+		"""
+		pass
