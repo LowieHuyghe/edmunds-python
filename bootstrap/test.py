9857f3f793480fb8bae4e4c0b7a950a9fde09c64
Lowie Huyghe <LowieHuyghe@users.noreply.github.com>
2016-10-26 01:06:03 +0200

    #145: Improved calling and bootstrapping of unitesting.

--- bootstrap/test.py
+++ bootstrap/test.py
@@ -4,9 +4,10 @@ from Edmunds.Application import Application
 import unittest
 import os
 import re
+import sys
 
 
-def bootstrap(rootFile):
+def bootstrap(rootFile, testLocations):
 	"""
 	Bootstrap the test-run
 	:return: 	The test-suit to test
@@ -17,18 +18,43 @@ def bootstrap(rootFile):
 
 	suites = []
 
-	for root, subdirs, files in os.walk('test'):
-		for file in files:
-			if file == '__init__.py':
-				continue
+	for testLocation in testLocations:
 
-			match = re.match(r'(^.*?)\.py$', file)
-			if not match:
-				continue
+		# Load tests from file or directory
+		if os.path.exists(testLocation):
+			# Directory
+			if os.path.isdir(testLocation):
+				for root, subdirs, files in os.walk(testLocation):
+					for file in files:
+						if file == '__init__.py':
+							continue
 
-			file = os.path.join(root, match.group(1))
-			testClassName = '.'.join(file.split('/'))
-			testClass = helpers.getClass(testClassName)
+						match = re.match(r'(^.*?)\.py$', file)
+						if not match:
+							continue
+
+						file = os.path.join(root, match.group(1))
+						testClassName = '.'.join(file.split('/'))
+						testClass = helpers.getClass(testClassName)
+
+						suite = unittest.TestLoader().loadTestsFromTestCase(testClass)
+						suites.append(suite)
+			# File
+			else:
+				match = re.match(r'(^.*?)\.py$', testLocation)
+				if not match:
+					continue
+
+				file = match.group(1)
+				testClassName = '.'.join(file.split('/'))
+				testClass = helpers.getClass(testClassName)
+
+				suite = unittest.TestLoader().loadTestsFromTestCase(testClass)
+				suites.append(suite)
+
+		# TestClassName given
+		else:
+			testClass = helpers.getClass(testLocation)
 
 			suite = unittest.TestLoader().loadTestsFromTestCase(testClass)
 			suites.append(suite)
