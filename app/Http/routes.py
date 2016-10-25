d6e795da76a903cd942873a895757154ba64e224
Lowie Huyghe <LowieHuyghe@users.noreply.github.com>
2016-10-26 00:27:49 +0200

    #145: Added bootstrap and entrypoint for testing.

--- app/Http/routes.py
+++ app/Http/routes.py
@@ -1,5 +1,4 @@
 
-
 def route(app):
 	"""
 	Define the routes in the application
--- bootstrap/edmunds.py
+++ bootstrap/edmunds.py
@@ -1,13 +1,16 @@
 
+import Edmunds.Support.helpers as helpers
 from Edmunds.Application import Application
 
 
-def bootstrap():
+def bootstrap(rootFile):
 	"""
 	Bootstrap the Application
 	:return: 	The bootstrapped application
 	:rtype:		Application
 	"""
+
+	rootDir = helpers.getDirFromFile(rootFile)
 
 	app = Application()
 
