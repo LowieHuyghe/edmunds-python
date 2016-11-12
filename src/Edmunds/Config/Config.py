c99342271fd6358690e94d5cdbbcfc057ddb1de1
Lowie Huyghe <LowieHuyghe@users.noreply.github.com>
2016-11-12 17:27:12 +0100

    #137: Improvement to config and loading of Application. Also load config file based on the namespace.

--- src/Edmunds/Config/Config.py
+++ src/Edmunds/Config/Config.py
@@ -1,11 +1,26 @@
 
 from flask.config import Config as FlaskConfig
+import os
 
 
 class Config(FlaskConfig):
 	"""
 	Config module
 	"""
+
+	def __init__(self, root_path, defaults=None):
+		"""
+		Initiate the dictionary
+		:param root_path: 	The root path
+		:type  root_path: 	str
+		:param defaults: 	The default
+		:type  defaults: 	mixed
+		"""
+
+		super(Config, self).__init__(root_path, defaults)
+
+		self.loaded_config = []
+
 
 	def __call__(self, mixed, default = None):
 		"""
@@ -17,6 +32,9 @@ class Config(FlaskConfig):
 		:return: 			Respectively the value and None
 		:rtype: 			mixed|None
 		"""
+
+		# Fetch the namespace and load them
+		self._load_namespace_from_call(mixed)
 
 		# Update dictionary
 		if isinstance(mixed, dict):
@@ -59,4 +77,33 @@ class Config(FlaskConfig):
 		:rtype: 		str
 		"""
 
-		return '.'.join(key.split('.')).upper()
+		return '_'.join(key.split('.')).upper()
+
+
+	def _load_namespace_from_call(self, mixed):
+		"""
+		Load namespace from call
+		:param mixed: 		Key of dictionary
+		:type mixed: 		dict|key
+		"""
+
+		namespaces = []
+		if isinstance(mixed, dict):
+			for key in mixed:
+				namespace = key.split('.')[0]
+				if key not in namespaces:
+					namespaces.append(namespace)
+		else:
+			namespace = mixed.split('.')[0]
+			namespaces.append(namespace)
+
+		for namespace in namespaces:
+			if namespace not in self.loaded_config:
+				self.loaded_config.append(namespace)
+				config_file = 'config/%s.py' % namespace
+				config_full_file_path = os.path.join(self.root_path, config_file)
+
+				print config_full_file_path
+
+				if os.path.isfile(config_full_file_path):
+					self.from_pyfile(config_file)
