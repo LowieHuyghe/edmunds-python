79ce004c6fda8a2f88454380fba897fb87ed4ddf
Lowie Huyghe <iam@lowiehuyghe.com>
2016-11-13 15:29:35 +0100

    #137: Added config implementation for environment.

--- src/Edmunds/Application.py
+++ src/Edmunds/Application.py
@@ -1,5 +1,6 @@
 
 from flask import Flask
+from Edmunds.Foundation.Concerns.RuntimeEnvironment import RuntimeEnvironment as ConcernsRuntimeEnvironment
 from Edmunds.Foundation.Concerns.ServiceProviders import ServiceProviders as ConcernsServiceProviders
 from Edmunds.Foundation.Concerns.Middleware import Middleware as ConcernsMiddleware
 from werkzeug.debug import DebuggedApplication
@@ -7,7 +8,7 @@ from app.Http import routes
 from Edmunds.Config.Config import Config
 
 
-class Application(Flask, ConcernsServiceProviders, ConcernsMiddleware):
+class Application(Flask, ConcernsRuntimeEnvironment, ConcernsServiceProviders, ConcernsMiddleware):
 	"""
 	The Edmunds Application
 	"""
@@ -15,24 +16,49 @@ class Application(Flask, ConcernsServiceProviders, ConcernsMiddleware):
 	config_class = Config
 
 
-	def __init__(self, import_name):
+	def __init__(self, import_name, config_dirs = None):
 		"""
 		Initialize the application
 		:param import_name: 	Import name
 		:type  import_name: 	str
+		:param config_dirs: 	Configuration directories
+		:type  config_dirs: 	list
 		"""
 
 		super(Application, self).__init__(import_name)
 
-		self.config.load_all()
-
-		self.debug = True
-		self.wsgi_app = DebuggedApplication(self.wsgi_app, True)
-
+		self._init_config(config_dirs)
 		self._init_service_providers()
 		self._init_middleware()
 
 		routes.route(self)
+
+
+	def _init_config(self, config_dirs = None):
+		"""
+		Initiate the configuration
+		:param config_dirs: 	Configuration directories
+		:type  config_dirs: 	list
+		"""
+
+		# Configuration directories
+		if config_dirs == None:
+			config_dirs = [
+				'lib/edmunds/src/config',
+				'config',
+			]
+
+		# Load config
+		self.config.load_all(config_dirs)
+
+		# Set to debug mode
+		if self.config('app.debug'):
+			self.debug = True
+			self.wsgi_app = DebuggedApplication(self.wsgi_app, True)
+
+		# Testing environment
+		if self.is_testing():
+			self.testing = True
 
 
 	def route(self, rule, **options):
