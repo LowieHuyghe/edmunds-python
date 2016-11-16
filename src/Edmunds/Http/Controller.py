65eea5586d99d681d6a83f655a879e0bd0289155
Lowie Huyghe <LowieHuyghe@users.noreply.github.com>
2016-11-16 22:57:16 +0100

    #152: Added params to the initialize function. Also added it to the test.

--- src/Edmunds/Http/Controller.py
+++ src/Edmunds/Http/Controller.py
@@ -4,8 +4,10 @@ class Controller(object):
 	The Controller
 	"""
 
-	def initialize(self):
+	def initialize(self, **params):
 		"""
 		Initialize the controller
+		:param params:		The arguments in the url
+		:type  params: 		dict
 		"""
 		pass
