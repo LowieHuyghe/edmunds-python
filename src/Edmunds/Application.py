25c601065ded9e224075188712c3a8c2f566f296
Lowie Huyghe <LowieHuyghe@users.noreply.github.com>
2016-11-17 01:18:05 +0100

    #143: Added initial stuff for handling exceptions.

--- src/Edmunds/Application.py
+++ src/Edmunds/Application.py
@@ -4,12 +4,13 @@ from Edmunds.Foundation.Concerns.RuntimeEnvironment import RuntimeEnvironment as
 from Edmunds.Foundation.Concerns.ServiceProviders import ServiceProviders as ConcernsServiceProviders
 from Edmunds.Foundation.Concerns.Middleware import Middleware as ConcernsMiddleware
 from Edmunds.Foundation.Concerns.RequestRouting import RequestRouting as ConcernsRequestRouting
+from Edmunds.Foundation.Concerns.ExceptionHandling import ExceptionHandling as ConcernsExceptionHandling
 from werkzeug.debug import DebuggedApplication
 from app.Http import routes
 from Edmunds.Config.Config import Config
 
 
-class Application(Flask, ConcernsRuntimeEnvironment, ConcernsServiceProviders, ConcernsMiddleware, ConcernsRequestRouting):
+class Application(Flask, ConcernsRuntimeEnvironment, ConcernsServiceProviders, ConcernsMiddleware, ConcernsRequestRouting, ConcernsExceptionHandling):
 	"""
 	The Edmunds Application
 	"""
@@ -32,6 +33,7 @@ class Application(Flask, ConcernsRuntimeEnvironment, ConcernsServiceProviders, C
 		self._init_service_providers()
 		self._init_middleware()
 		self._init_request_routing()
+		self._init_exception_handling()
 
 		routes.route(self)
 
