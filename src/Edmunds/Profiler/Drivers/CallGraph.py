85d8faa0efe7cbfe2a14a5a5698a18c85dbbf43f
Lowie Huyghe <iam@lowiehuyghe.com>
2016-12-12 19:46:48 +0100

    #156: Improved suffix for profiler files.

--- src/Edmunds/Profiler/Drivers/CallGraph.py
+++ src/Edmunds/Profiler/Drivers/CallGraph.py
@@ -9,7 +9,7 @@ class CallGraph(BaseDriver):
 	CallGraph driver
 	"""
 
-	def __init__(self, app, directory, prefix = ''):
+	def __init__(self, app, directory, prefix = '', suffix = ''):
 		"""
 		Initiate the instance
 		:param app: 			The application
@@ -18,12 +18,15 @@ class CallGraph(BaseDriver):
 		:type  directory:		str
 		:param prefix: 			The prefix for storing
 		:type  prefix: 			str
+		:param suffix: 			The suffix for storing
+		:type  suffix: 			str
 		"""
 
 		super(CallGraph, self).__init__(app)
 
 		self._profile_dir = directory
 		self._prefix = prefix
+		self._suffix = suffix
 
 
 	def process(self, profiler, start, end, environment, suggestive_file_name):
@@ -41,10 +44,11 @@ class CallGraph(BaseDriver):
 		:type  suggestive_file_name: 	str
 		"""
 
-		filename = os.path.join(self._profile_dir, self._prefix + suggestive_file_name + '.callgraph')
+		filename = self._prefix + suggestive_file_name + self._suffix
+		filepath = os.path.join(self._profile_dir, filename)
 
 		converter = CalltreeConverter(profiler.getstats())
-		f = self._app.write_stream(filename)
+		f = self._app.write_stream(filepath)
 
 		try:
 			converter.output(f)
