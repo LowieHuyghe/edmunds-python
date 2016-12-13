e057c47e92829b67522b7cf173af4fdd203f2939
Lowie Huyghe <LowieHuyghe@users.noreply.github.com>
2016-12-13 23:53:43 +0100

    #156 #144: Implemented base manager with extra functionality. Made it thread safe.

--- src/Edmunds/Log/LogManager.py
+++ src/Edmunds/Log/LogManager.py
@@ -1,4 +1,5 @@
 
+from Edmunds.Foundation.Patterns.Manager import Manager
 import Edmunds.Support.helpers as helpers
 from Edmunds.Log.Drivers.File import File
 from Edmunds.Log.Drivers.TimedFile import TimedFile
@@ -7,7 +8,7 @@ from Edmunds.Log.Drivers.Stream import Stream
 import os
 
 
-class LogManager(object):
+class LogManager(Manager):
 	"""
 	Log Manager
 	"""
@@ -19,83 +20,9 @@ class LogManager(object):
 		:type  app: 	Edmunds.Application
 		"""
 
-		self._app = app
+		super(LogManager, self).__init__(app, app.config('app.log.instances'))
+
 		self._default_log_dir = self._app.storage_path('logs')
-
-		self._load()
-
-
-	def get(self, name = None):
-		"""
-		Get the instance
-		:param name: 	The name of the instance
-		:type  name:	str
-		:return:		The driver
-		:rtype:			BaseDriver
-		"""
-
-		if len(self._instances) == 0:
-			raise RuntimeError('No log-instances declared.')
-
-		if name is None:
-			name = self._instances.keys()[0]
-
-		return self._instances[name]
-
-
-	def all(self):
-		"""
-		Get all the instances
-		"""
-
-		return self._instances.values()
-
-
-	def _load(self):
-		"""
-		Load all the instances
-		"""
-
-		self._instances = {}
-
-		instances_config = self._app.config('app.log.instances')
-		for instance_config in instances_config:
-			name = instance_config['name']
-			if name in self._instances:
-				raise RuntimeError('Redeclaring log-instance with name "%s"' % name)
-
-			self._instances[name] = self._resolve(name)
-
-
-	def _resolve(self, name):
-		"""
-		Resolve the instance
-		:param name:	The name of the instance
-		:type  name:	str
-		:return:		The driver
-		:rtype:			BaseDriver
-		"""
-
-		# Fetch config
-		instances_config = self._app.config('app.log.instances')
-
-		# Pick one
-		instance_config = None
-		for instances_config_item in instances_config:
-			if instances_config_item['name'] == name:
-				instance_config = instances_config_item
-				break
-
-		# Check if there is one
-		if instance_config is None:
-			raise RuntimeError('There is no log-instance declared in the config with name "%s"' % name)
-
-		# Make the driver
-		driver_class = instance_config['driver']
-		method_name = '_create_%s' % helpers.snake_case(driver_class.__name__)
-		driver = getattr(self, method_name)(instance_config)
-
-		return driver
 
 
 	def _create_file(self, config):
