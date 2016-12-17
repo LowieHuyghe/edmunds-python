c3f8df9fc413db79a803916c6d0676472e422e9f
Lowie Huyghe <LowieHuyghe@users.noreply.github.com>
2016-12-17 16:32:28 +0100

    #157: Added application to the controller.

--- src/Edmunds/Http/Controller.py
+++ src/Edmunds/Http/Controller.py
@@ -4,6 +4,14 @@ class Controller(object):
 	The Controller
 	"""
 
+	def __init__(self, app):
+		"""
+		Initialize the controller
+		"""
+
+		self._app = app
+
+
 	def initialize(self, **params):
 		"""
 		Initialize the controller
