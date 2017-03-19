5b11b8f0c7c2883964338ca7d4c4694c82016130
Lowie Huyghe <iam@lowiehuyghe.com>
2017-03-19 19:12:44 +0100

    #21: Added coverage for copy and error fails scenarios.

--- tests/storage/drivers/testfile.py
+++ tests/storage/drivers/testfile.py
@@ -2,6 +2,7 @@
 from tests.testcase import TestCase
 import edmunds.support.helpers as helpers
 import os
+import mock
 
 
 class TestFile(TestCase):
@@ -96,3 +97,103 @@ class TestFile(TestCase):
         self.assert_true(app.fs().exists('nice2.txt'))
         self.assert_true(app.fs().delete('nice2.txt'))
         self.assert_false(app.fs().exists('nice2.txt'))
+
+    def test_copy_error(self):
+        """
+        Test error scenario for copy
+        :return:    void
+        """
+
+        string = helpers.random_str(20)
+
+        # Write config
+        self.write_config([
+            "from edmunds.storage.drivers.file import File \n",
+            "from logging import WARNING \n",
+            "APP = { \n",
+            "   'storage': { \n",
+            "       'instances': [ \n",
+            "           { \n",
+            "               'name': 'file',\n",
+            "               'driver': File,\n",
+            "               'directory': '%s',\n" % self.storage_directory,
+            "               'prefix': '%s',\n" % self.prefix,
+            "           }, \n",
+            "       ], \n",
+            "   }, \n",
+            "   'log': { \n",
+            "       'instances': [ \n",
+            "       ], \n",
+            "   }, \n",
+            "} \n",
+            ])
+
+        # Create app
+        app = self.create_application()
+        directory = app.fs()._get_processed_path(None)
+        self.clear_paths.append(directory)
+
+        # Write
+        stream = app.fs().write_stream('nice.txt')
+        try:
+            stream.write(string)
+        finally:
+            stream.close()
+
+        # Copy
+        with mock.patch('shutil.copy2', side_effect=IOError()):
+            self.assert_false(app.fs().copy('nice.txt', 'nice2.txt'))
+            self.assert_false(app.fs().copy('nice.txt', 'nice2.txt', raise_errors=False))
+
+            with self.assert_raises(IOError):
+                app.fs().copy('nice.txt', 'nice2.txt', raise_errors=True)
+
+    def test_delete_error(self):
+        """
+        Test delete scenario for error
+        :return:    void
+        """
+
+        string = helpers.random_str(20)
+
+        # Write config
+        self.write_config([
+            "from edmunds.storage.drivers.file import File \n",
+            "from logging import WARNING \n",
+            "APP = { \n",
+            "   'storage': { \n",
+            "       'instances': [ \n",
+            "           { \n",
+            "               'name': 'file',\n",
+            "               'driver': File,\n",
+            "               'directory': '%s',\n" % self.storage_directory,
+            "               'prefix': '%s',\n" % self.prefix,
+            "           }, \n",
+            "       ], \n",
+            "   }, \n",
+            "   'log': { \n",
+            "       'instances': [ \n",
+            "       ], \n",
+            "   }, \n",
+            "} \n",
+            ])
+
+        # Create app
+        app = self.create_application()
+        directory = app.fs()._get_processed_path(None)
+        self.clear_paths.append(directory)
+
+        # Write
+        stream = app.fs().write_stream('nice.txt')
+        try:
+            stream.write(string)
+        finally:
+            stream.close()
+
+        # Copy
+        with mock.patch('os.remove', side_effect=IOError()):
+            self.assert_false(app.fs().delete('nice.txt'))
+            self.assert_false(app.fs().delete('nice.txt', raise_errors=False))
+
+            with self.assert_raises(IOError):
+                app.fs().delete('nice.txt', raise_errors=True)
