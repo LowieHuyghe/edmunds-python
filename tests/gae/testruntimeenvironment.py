c6a13b75545201f762cf4d2ce9f86deb540bfc72
Lowie Huyghe <iam@lowiehuyghe.com>
2017-03-28 23:59:32 +0200

    #21: Test env for gae runtime detection.

--- tests/gae/testruntimeenvironment.py
+++ tests/gae/testruntimeenvironment.py
@@ -1,6 +1,7 @@
 
 from tests.gae.gaetestcase import GaeTestCase
 from edmunds.application import Application
+import os
 if GaeTestCase.can_run():
     from edmunds.gae.runtimeenvironment import RuntimeEnvironment
     from edmunds.gae.application import Application as GaeApplication
@@ -27,6 +28,31 @@ class TestRuntimeEnvironment(GaeTestCase):
 
         self.testbed.activate()
 
+        self.assert_true(env.is_gae())
+
+    def test_is_gae_env(self):
+        """
+        Test is gae with env
+        :return:    void
+        """
+
+        self.testbed.deactivate()
+        env = RuntimeEnvironment()
+        self.assert_false(env.is_gae())
+
+        os.environ['CURRENT_VERSION_ID'] = self.rand_str()
+        self.assert_false(env.is_gae())
+
+        os.environ['AUTH_DOMAIN'] = self.rand_str()
+        self.assert_false(env.is_gae())
+
+        os.environ['SERVER_SOFTWARE'] = self.rand_str()
+        self.assert_false(env.is_gae())
+
+        os.environ['SERVER_SOFTWARE'] = 'Development/%s' % self.rand_str()
+        self.assert_true(env.is_gae())
+
+        os.environ['SERVER_SOFTWARE'] = 'Google App Engine/%s' % self.rand_str()
         self.assert_true(env.is_gae())
 
     def test_testcase_app(self):
