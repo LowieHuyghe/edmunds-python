94f9b05f8e8c452a4f43a93e4fa5a6bee2a0cb58
Lowie Huyghe <LowieHuyghe@users.noreply.github.com>
2017-06-06 02:25:48 +0200

    #36: Added some commands and functionality for command line usage.

---
+++ edmunds/console/command.py
@@ -0,0 +1,11 @@
+
+from flask_script import Command as FlaskCommand
+from flask_script import Option as FlaskOption
+
+
+class Command(FlaskCommand):
+    pass
+
+
+class Option(FlaskOption):
+    pass
