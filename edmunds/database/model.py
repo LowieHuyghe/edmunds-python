932bb79b34274bf4fac1fde8808c0ec11bbc6c0e
Lowie Huyghe <LowieHuyghe@users.noreply.github.com>
2017-06-08 00:38:34 +0200

    #11: Added package-loader to migrateserviceprovider to load all models when migrating

--- edmunds/database/model.py
+++ edmunds/database/model.py
@@ -2,4 +2,5 @@
 from edmunds.database.databasemanager import DatabaseManager
 
 
-Model = DatabaseManager.get_sql_alchemy_instance().Model
+db = DatabaseManager.get_sql_alchemy_instance()
+Model = db.Model
