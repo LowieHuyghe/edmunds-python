b282eb9bd7f020945274d91261a5a24c99ab9f4a
Lowie Huyghe <LowieHuyghe@users.noreply.github.com>
2017-06-27 01:13:18 +0200

    #13: Improved database-model for running queries and fetch a session.

--- edmunds/database/model.py
+++ edmunds/database/model.py
@@ -1,5 +1,7 @@
 
+from edmunds.application import current_app
 import sqlalchemy.orm
+
 
 mapper = sqlalchemy.orm.mapper
 relationship = sqlalchemy.orm.relationship
@@ -7,4 +9,37 @@ backref = sqlalchemy.orm.backref
 
 
 class Model(object):
-    pass
+    """
+    Model base class
+    """
+
+    __table__ = None
+
+    @classmethod
+    def session(cls, name=None):
+        """
+        Session
+        :param name:    Name of the bind 
+        :return:        sqlalchemy.orm.scoping.scoped_session
+        """
+
+        if name is None \
+                and cls.__table__ is not None \
+                and hasattr(cls.__table__, 'info') \
+                and cls.__table__.info \
+                and 'bind_key' in cls.__table__.info \
+                and cls.__table__.info['bind_key']:
+            name = cls.__table__.info['bind_key']
+
+        return current_app.database_session(name=name)
+
+    @classmethod
+    def query(cls, name=None):
+        """
+        Query
+        :param name:    Name of the bind 
+        :return:        sqlalchemy.orm.scoping.query
+        """
+
+        Session = cls.session(name=name)
+        return Session.query(cls)
