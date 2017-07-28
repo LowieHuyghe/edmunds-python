daa27cb7e124b817e7317dc5b686f85d20a4e4d1
Lowie Huyghe <LowieHuyghe@users.noreply.github.com>
2017-07-29 01:20:15 +0200

    #45: Added location drivers using MaxMind. Added provider and manager.

---
+++ edmunds/localization/location/drivers/maxmindenterprisedatabase.py
@@ -0,0 +1,32 @@
+
+from geoip2.database import Reader
+from edmunds.localization.location.drivers.basedriver import BaseDriver
+
+
+class MaxMindEnterpriseDatabase(BaseDriver):
+    """
+    MaxMind Enterprise Database
+    """
+
+    def __init__(self, database):
+        """
+        Constructor
+        :param database:    Location of the enterprise-database
+        """
+        self._reader = Reader(database)
+
+    def insights(self, ip):
+        """
+        Get insights in ip
+        :param ip:  The ip
+        :return:    Insights
+        :rtype:     geoip2.models.City
+        """
+        return self._reader.enterprise(ip)
+
+    def close(self):
+        """
+        Close the reader
+        :return:    void
+        """
+        self._reader.close()
