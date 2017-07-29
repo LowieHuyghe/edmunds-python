2b48ef7df938fd2de9747eed1951a3aff569e36c
Lowie Huyghe <LowieHuyghe@users.noreply.github.com>
2017-07-29 14:58:37 +0200

    #45: Applied prefix parameter to all filesystem functions. Also added functionality to tests.

--- edmunds/storage/drivers/basedriver.py
+++ edmunds/storage/drivers/basedriver.py
@@ -18,33 +18,37 @@ class BaseDriver(ABC):
         self._app = app
 
     @abc.abstractmethod
-    def write_stream(self, path, append=False):
+    def write_stream(self, path, append=False, prefix=None):
         """
         Get a write stream to a certain path
         :param path:    The path to the file
         :type  path:    str
         :param append:  Append to the file
         :type  append:  bool
+        :param prefix:  The prefix
+        :type  prefix:  str
         :return:        The write stream
         :rtype:         Stream
         """
         pass
 
     @abc.abstractmethod
-    def read_stream(self, path, raise_errors=False):
+    def read_stream(self, path, raise_errors=False, prefix=None):
         """
         Get a read stream to a certain path
         :param path:            The path to the file
         :type  path:            str
         :param raise_errors:    Raise the errors
         :type  raise_errors:    bool
+        :param prefix:          The prefix
+        :type  prefix:          str
         :return:                The read stream
         :rtype:                 Stream
         """
         pass
 
     @abc.abstractmethod
-    def copy(self, path, new_path, raise_errors=False):
+    def copy(self, path, new_path, raise_errors=False, prefix=None):
         """
         Copy a certain path
         :param path:            The path to the file
@@ -53,30 +57,36 @@ class BaseDriver(ABC):
         :type  new_path:        str
         :param raise_errors:    Raise the errors
         :type  raise_errors:    bool
+        :param prefix:          The prefix
+        :type  prefix:          str
         :return:                Success
         :rtype:                 bool
         """
         pass
 
     @abc.abstractmethod
-    def delete(self, path, raise_errors=False):
+    def delete(self, path, raise_errors=False, prefix=None):
         """
         Delete a certain path
         :param path:            The path to the file
         :type  path:            str
         :param raise_errors:    Raise the errors
         :type  raise_errors:    bool
+        :param prefix:          The prefix
+        :type  prefix:          str
         :return:                Success
         :rtype:                 bool
         """
         pass
 
     @abc.abstractmethod
-    def exists(self, path):
+    def exists(self, path, prefix=None):
         """
         Check if a certain path exists
         :param path:    The path to the file
         :type  path:    str
+        :param prefix:  The prefix
+        :type  prefix:  str
         :return:        Exists
         :rtype:         bool
         """
