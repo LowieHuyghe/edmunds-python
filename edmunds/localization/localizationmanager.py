1a5ddd87a96c16501726f76db1bb189fdb4ce65a
Lowie Huyghe <LowieHuyghe@users.noreply.github.com>
2017-07-29 13:54:56 +0200

    #45: Moved to other package for offline timezone lookup. Fixed locationmanager. Added test for GoogleAppEngine.

--- edmunds/localization/localizationmanager.py
+++ edmunds/localization/localizationmanager.py
@@ -8,16 +8,20 @@ class LocalizationManager(object):
         """
         self._app = app
 
-    def location(self):
+    def location(self, name=None, no_instance_error=False):
         """
         The location manager
-        :return:    The location manager
-        :rtype:     edmunds.localization.location.locationmanager.LocationManager
+        :param name:                The name of the session instance
+        :type  name:                str
+        :param no_instance_error:   Error when no instance
+        :type  no_instance_error:   bool
+        :return:                    A location driver
+        :rtype:                     edmunds.localization.location.drivers.basedriver.BaseDriver
         """
 
         # Enabled?
         if not self._app.config('app.localization.location.enabled', False):
             return None
 
-        # Return manager
-        return self._app.extensions['edmunds.localization.location']
+        # Return driver
+        return self._app.extensions['edmunds.localization.location'].get(name, no_instance_error=no_instance_error)
