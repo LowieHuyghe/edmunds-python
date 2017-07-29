30f4ec4563c553f3896c4c70ce3071c1d56b6e5e
Lowie Huyghe <iam@lowiehuyghe.com>
2017-07-29 12:47:41 +0200

    #45: Added tests to test localization manager and service provider with no submanagers of disabled submanagers.

--- tests/localization/providers/testlocalizationserviceprovider.py
+++ tests/localization/providers/testlocalizationserviceprovider.py
@@ -23,6 +23,41 @@ class TestLocalizationServiceProvider(TestCase):
         # Should also have registered location-manager
         self.assert_not_in('edmunds.localization.location', self.app.extensions)
 
+    def test_provider_with_submanagers_disabled(self):
+        """
+        Test the provider with submanagers disabled
+        :return:    void
+        """
+
+        # Write config
+        self.write_config([
+            "from edmunds.localization.location.drivers.googleappengine import GoogleAppEngine \n",
+            "APP = { \n",
+            "   'localization': { \n",
+            "       'location': { \n",
+            "           'enabled': False, \n",
+            "           'instances': [ \n",
+            "               { \n",
+            "                   'name': 'gae',\n",
+            "                   'driver': GoogleAppEngine,\n",
+            "               }, \n",
+            "           ], \n",
+            "       }, \n",
+            "   }, \n",
+            "} \n",
+            ])
+
+        # Create app
+        app = self.create_application()
+
+        # Test extension
+        self.assert_in('edmunds.localization', app.extensions)
+        self.assert_is_not_none(app.extensions['edmunds.localization'])
+        self.assert_is_instance(app.extensions['edmunds.localization'], LocalizationManager)
+
+        # Should also have registered location-manager
+        self.assert_not_in('edmunds.localization.location', app.extensions)
+
     def test_provider_with_submanagers(self):
         """
         Test the provider with submanagers
--- tests/localization/testlocalizationmanager.py
+++ tests/localization/testlocalizationmanager.py
@@ -9,16 +9,55 @@ class TestLocalizationManager(TestCase):
     Test the Localization Manager
     """
 
-    def test_location(self):
+    def test_location_not_defined(self):
         """
-        Test location
+        Test location not defined
         :return:    void
         """
 
         manager = self.app.localization()
         self.assert_is_instance(manager, LocalizationManager)
+
         # No settings for the manager
         self.assert_is_none(manager.location())
+
+    def test_location_disabled(self):
+        """
+        Test location disabled
+        :return:    void
+        """
+
+        # Write location settings
+        self.write_config([
+            "from edmunds.localization.location.drivers.googleappengine import GoogleAppEngine \n",
+            "APP = { \n",
+            "   'localization': { \n",
+            "       'location': { \n",
+            "           'enabled': False, \n",
+            "           'instances': [ \n",
+            "               { \n",
+            "                   'name': 'gae',\n",
+            "                   'driver': GoogleAppEngine,\n",
+            "               }, \n",
+            "           ], \n",
+            "       }, \n",
+            "   }, \n",
+            "} \n",
+        ])
+        app = self.create_application()
+
+        # New manager
+        manager = app.localization()
+        self.assert_is_instance(manager, LocalizationManager)
+
+        # with settings for the manager
+        self.assert_is_none(manager.location())
+
+    def test_location(self):
+        """
+        Test location
+        :return:    void
+        """
 
         # Write location settings
         self.write_config([
@@ -38,9 +77,11 @@ class TestLocalizationManager(TestCase):
             "} \n",
         ])
         app = self.create_application()
+
         # New manager
         manager = app.localization()
         self.assert_is_instance(manager, LocalizationManager)
+
         # with settings for the manager
         self.assert_is_not_none(manager.location())
         self.assert_is_instance(manager.location(), LocationManager)
