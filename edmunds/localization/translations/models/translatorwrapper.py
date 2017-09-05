60a3b4093be178577bf0cec7086a8a63698af76c
Lowie Huyghe <LowieHuyghe@users.noreply.github.com>
2017-09-06 01:04:06 +0200

    #48: Added fallback locale to translator,

--- edmunds/localization/translations/models/translatorwrapper.py
+++ edmunds/localization/translations/models/translatorwrapper.py
@@ -1,17 +1,27 @@
 
+from edmunds.localization.translations.exceptions.translationerror import TranslationError
+from edmunds.localization.translations.exceptions.sentencefillererror import SentenceFillerError
+
+
 class TranslatorWrapper(object):
 
-    def __init__(self, translator, locale):
+    def __init__(self, app, translator, locale, locale_fallback):
         """
         Constructor
-        :param translator:  The translator-driver
-        :type translator:   edmunds.localization.translations.drivers.basedriver.BaseDriver
-        :param locale:      The locale
-        :type locale:       babel.core.Locale
+        :param app:             The application
+        :type app:              edmunds.application.Application
+        :param translator:      The translator-driver
+        :type translator:       edmunds.localization.translations.drivers.basedriver.BaseDriver
+        :param locale:          The locale
+        :type locale:           babel.core.Locale
+        :param locale_fallback: The fallback locale
+        :type locale_fallback:  babel.core.Locale
         """
 
+        self.app = app
         self.translator = translator
         self.locale = locale
+        self.locale_fallback = locale_fallback
 
     def get(self, key, parameters=None):
         """
@@ -24,4 +34,11 @@ class TranslatorWrapper(object):
         :type:              str
         """
 
-        return self.translator.get(self.locale, key, parameters=parameters)
+        try:
+            return self.translator.get(self.locale, key, parameters=parameters)
+        except TranslationError as e:
+            self.app.logger.error(e)
+        except SentenceFillerError as e:
+            self.app.logger.error(e)
+
+        return self.translator.get(self.locale_fallback, key, parameters=parameters)
