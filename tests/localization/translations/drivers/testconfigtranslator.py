2f70a2d13241a6f8690106782b01b9bb8f6f06e8
Lowie Huyghe <LowieHuyghe@users.noreply.github.com>
2017-09-12 00:12:49 +0200

    #48: Added better descriptive errors to the configtranslator. Improved testing regarding this. Test for sentence error in translator wrapper.

--- tests/localization/translations/drivers/testconfigtranslator.py
+++ tests/localization/translations/drivers/testconfigtranslator.py
@@ -3,6 +3,7 @@ from tests.testcase import TestCase
 from edmunds.localization.translations.drivers.configtranslator import ConfigTranslator
 from edmunds.localization.translations.models.translatorwrapper import TranslatorWrapper
 from edmunds.localization.translations.exceptions.translationerror import TranslationError
+from edmunds.localization.translations.exceptions.sentencefillererror import SentenceFillerError
 
 
 class TestConfigTranslator(TestCase):
@@ -52,6 +53,35 @@ class TestConfigTranslator(TestCase):
     def test_get_unknown_key(self):
         """
         Test get unknown key
+        :return:    void
+        """
+
+        rule = '/' + self.rand_str(20)
+
+        # Write config and create app
+        self.write_config(self.config)
+        app = self.create_application()
+        fallback_locale_str = app.config('app.localization.locale.fallback')
+
+        data = [
+            ('en', 'beautiful', {}),
+            ('nl', 'beautiful', {}),
+            ('nl_BE', 'beautiful', {}),
+        ]
+
+        for locale_str, key, params in data:
+            with app.test_request_context(rule):
+                # Fetch driver
+                driver = app.localization().translator(None, given_locale_strings=[locale_str])
+                self.assert_is_instance(driver, TranslatorWrapper)
+                self.assert_is_instance(driver.translator, ConfigTranslator)
+
+                with self.assert_raises_regexp(SentenceFillerError, 'Param "name" could not be replaced. \(locale "%s" and key "%s"\)' % (fallback_locale_str, key)):
+                    driver.get(key, params)
+
+    def test_get_errors(self):
+        """
+        Test get errors
         :return:    void
         """
 
