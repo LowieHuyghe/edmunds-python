59df33709de871b2a91e3b33850bffff9194b6e2
Lowie Huyghe <LowieHuyghe@users.noreply.github.com>
2017-10-08 23:07:58 +0200

    Moved gaetestcase so it can be used externally.

---
+++ edmunds/gae/gaetestcase.py
@@ -0,0 +1,44 @@
+
+from tests.testcase import TestCase
+import sys
+def gae_can_run():
+    return sys.version_info < (3, 0)
+if gae_can_run():
+    from google.appengine.ext import testbed
+
+
+class GaeTestCase(TestCase):
+
+    @staticmethod
+    def can_run():
+        """
+        Check if can run test
+        :return:    Boolean
+        """
+
+        return gae_can_run()
+
+    def set_up(self):
+        """
+        Set up the test case
+        """
+
+        if not self.can_run():
+            self.skip("Google Cloud SDK doesn't run in Python 3+")
+
+        self.testbed = testbed.Testbed()
+        self.testbed.activate()
+
+        super(GaeTestCase, self).set_up()
+
+    def tear_down(self):
+        """
+        Tear down the test case
+        """
+
+        super(GaeTestCase, self).set_up()
+
+        try:
+            self.testbed.deactivate()
+        except testbed.NotActivatedError:
+            pass
