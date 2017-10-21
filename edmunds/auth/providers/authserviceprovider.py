f15f8df9c4e3635616812a0e6d0fd1728f99cfad
Lowie Huyghe <LowieHuyghe@users.noreply.github.com>
2017-10-21 14:58:28 +0200

    #16: Changed auth to use manager to load instances

--- edmunds/auth/providers/authserviceprovider.py
+++ edmunds/auth/providers/authserviceprovider.py
@@ -1,7 +1,6 @@
 
 from edmunds.support.serviceprovider import ServiceProvider
-from flask_security import Security, SQLAlchemyUserDatastore
-from edmunds.database.table import db
+from edmunds.auth.authmanager import AuthManager
 
 
 class AuthServiceProvider(ServiceProvider):
@@ -18,20 +17,8 @@ class AuthServiceProvider(ServiceProvider):
         if not self.app.config('app.auth.enabled', False):
             return
 
-        # Database enabled?
-        if not self.app.config('app.database.enabled', False):
-            raise RuntimeError("Auth requires database to be enabled.")
+        # Make manager and load instance
+        manager = AuthManager(self.app)
 
-        # User and Role model
-        user_class = self.app.config('app.auth.models.user', False)
-        role_class = self.app.config('app.auth.models.role', False)
-        if user_class is None \
-                or role_class is None:
-            raise RuntimeError("Auth-config is missing some configuration ('app.auth.models.user' and 'app.auth.models.role' are required).")
-
-        # Setup Flask-Security
-        userdatastore = SQLAlchemyUserDatastore(db, user_class, role_class)
-        security = Security(self.app, userdatastore)
-
-        self.app.extensions['edmunds.auth.userdatastore'] = userdatastore
-        self.app.extensions['edmunds.auth.security'] = security
+        # Assign to extensions
+        self.app.extensions['edmunds.auth'] = manager
