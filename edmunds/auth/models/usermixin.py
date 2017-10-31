8bf7ce0d119d8a7595a2b45b9977b2974712fa2a
Lowie Huyghe <LowieHuyghe@users.noreply.github.com>
2017-10-31 12:56:00 +0100

    #16: Added config auth. Added test for token auth.

--- edmunds/auth/models/usermixin.py
+++ edmunds/auth/models/usermixin.py
@@ -11,16 +11,18 @@ class UserMixin(FlaskSecurityUserMixin, object):
     # __tablename__ = 'user'
     # __bind_key__ = 'users_database'
 
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(255), unique=True)
+    id = db.Column(db.Integer, nullable=False, primary_key=True)
+    email = db.Column(db.String(255), nullable=False, unique=True)
     password = db.Column(db.String(255))
     active = db.Column(db.Boolean())
     confirmed_at = db.Column(db.DateTime())
     last_login_at = db.Column(db.DateTime())
     current_login_at = db.Column(db.DateTime())
-    last_login_ip = db.Column(db.String(255))
-    current_login_ip = db.Column(db.String(255))
+    # Why 45 characters for IP Address ?
+    # See http://stackoverflow.com/questions/166132/maximum-length-of-the-textual-representation-of-an-ipv6-address/166157#166157
+    last_login_ip = db.Column(db.String(45))
+    current_login_ip = db.Column(db.String(45))
     login_count = db.Column(db.Integer)
 
     def __repr__(self):
-        return '<User id="%s"/>' % self.id
+        return '<User id="%s" email="%s"/>' % (self.id, self.email)
