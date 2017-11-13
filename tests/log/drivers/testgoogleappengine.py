744fbdb71d5c1d6370eb87c3ff379532061e77fb
Lowie Huyghe <LowieHuyghe@users.noreply.github.com>
2017-11-13 22:50:46 +0100

    #83: Improved and fixed Google App Engine logger.

--- tests/log/drivers/testgoogleappengine.py
+++ tests/log/drivers/testgoogleappengine.py
@@ -1,5 +1,11 @@
 
 from tests.gaetestcase import GaeTestCase
+import os
+from logging import StreamHandler, ERROR
+try:
+   from cStringIO import StringIO
+except ImportError:
+   from io import StringIO
 
 
 class TestGoogleAppEngine(GaeTestCase):
@@ -16,23 +22,25 @@ class TestGoogleAppEngine(GaeTestCase):
 
         self.testbed.init_logservice_stub()
 
-    def test_stream(self):
+    def tear_down(self):
         """
-        Test the stream
+        Tear down the test case
         """
 
-        info_string = 'info_%s' % self.rand_str(20)
-        warning_string = 'warning_%s' % self.rand_str(20)
-        error_string = 'error_%s' % self.rand_str(20)
+        super(TestGoogleAppEngine, self).tear_down()
+
+        if 'SERVER_SOFTWARE' in os.environ:
+            del os.environ['SERVER_SOFTWARE']
+
+    def test_logger(self):
+        """
+        Test the logger
+        """
 
         # Write config
         self.write_config([
             "from edmunds.log.drivers.googleappengine import GoogleAppEngine \n",
-            "from logging import WARNING \n",
-            "try: \n",
-            "   from cStringIO import StringIO \n",
-            "except ImportError: \n",
-            "   from io import StringIO \n",
+            "from logging import ERROR \n",
             "APP = { \n",
             "   'debug': False, \n",
             "   'log': { \n",
@@ -41,26 +49,70 @@ class TestGoogleAppEngine(GaeTestCase):
             "           { \n",
             "               'name': 'googleappengine',\n",
             "               'driver': GoogleAppEngine,\n",
-            "               'stream': StringIO(),\n",
-            "               'level': WARNING,\n"
-            "           }, \n",
-            "           { \n",
-            "               'name': 'googleappengine2',\n",
-            "               'driver': GoogleAppEngine,\n",
-            "               'level': WARNING,\n"
-            "               'format': '%(message)s',\n",
+            "               'level': ERROR,\n"
             "           }, \n",
             "       ], \n",
             "   }, \n",
             "} \n",
         ])
+        os.environ['SERVER_SOFTWARE'] = 'Google App Engine/%s' % self.rand_str()
 
         # Create app and fetch stream
         app = self.create_application()
-        stream = app.config('app.log.instances')[0]['stream']
 
-        # Add route
+        from google.appengine.api.app_logging import AppLogsHandler
+        logger = app.logger.handlers[-1]
+        self.assert_is_instance(logger, AppLogsHandler)
+        self.assert_equal(ERROR, logger.level)
+
+        self.assert_is_none(logger.development_handler)
+
+    def test_logger_development(self):
+        """
+        Test the logger in development
+        """
+
         rule = '/' + self.rand_str(20)
+        info_string = 'info_%s' % self.rand_str(20)
+        warning_string = 'warning_%s' % self.rand_str(20)
+        error_string = 'error_%s' % self.rand_str(20)
+
+        # Write config
+        self.write_config([
+            "from edmunds.log.drivers.googleappengine import GoogleAppEngine \n",
+            "from logging import ERROR \n",
+            "APP = { \n",
+            "   'debug': False, \n",
+            "   'log': { \n",
+            "       'enabled': True, \n",
+            "       'instances': [ \n",
+            "           { \n",
+            "               'name': 'googleappengine',\n",
+            "               'driver': GoogleAppEngine,\n",
+            "               'level': ERROR,\n"
+            "           }, \n",
+            "       ], \n",
+            "   }, \n",
+            "} \n",
+        ])
+        os.environ['SERVER_SOFTWARE'] = 'Development/%s' % self.rand_str()
+
+        # Create app and fetch stream
+        app = self.create_application()
+
+        from google.appengine.api.app_logging import AppLogsHandler
+        logger = app.logger.handlers[-1]
+        self.assert_is_instance(logger, AppLogsHandler)
+        self.assert_equal(ERROR, logger.level)
+
+        self.assert_is_not_none(logger.development_handler)
+        self.assert_is_instance(logger.development_handler, StreamHandler)
+        self.assert_equal(ERROR, logger.development_handler.level)
+
+        # Test streaming
+        stream = StringIO()
+        logger.development_handler.stream = stream
+
         @app.route(rule)
         def handle_route():
             app.logger.info(info_string)
@@ -80,5 +132,5 @@ class TestGoogleAppEngine(GaeTestCase):
 
             # Check stream
             self.assert_not_in(info_string, stream.getvalue())
-            self.assert_in(warning_string, stream.getvalue())
+            self.assert_not_in(warning_string, stream.getvalue())
             self.assert_in(error_string, stream.getvalue())
